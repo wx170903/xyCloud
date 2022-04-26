f5da06b2030cd3e00dcedb571bf368ed8046123e
wx170903 <384313798@qq.com>
2022-04-26 17:55:35 +0800

    scss调试

--- src/compontents/head.tsx
+++ src/compontents/head.tsx
@@ -1,20 +1,23 @@
-import React,{useCallback} from 'react';
-
-import {useNavigate} from "react-router-dom";
+import React from 'react';
+import style from '../css/component.module.scss';
+import { useNavigate } from "react-router-dom";
 
 function Head() {
     const navigate = useNavigate();
 
-    function handleClick(n:string) {
+    function handleClick(n: string) {
         navigate(n);
     }
 
     return (
-        <div>
-            <div onClick={()=>handleClick('/cloud')}>cloud1</div>
-            <div onClick={()=>handleClick('/note')}>note</div>
+        <div className={style.head_container}>
+            <div className={style.logo_wrapper}>
+                <img className='logImg' src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80" alt="" />
+                <h2 className="text">xyCloud</h2>
+            </div>
+            <div className='link_btn' onClick={() => handleClick('/cloud')}>云盘</div>
+            <div className='link_btn' onClick={() => handleClick('/note')}>笔记</div>
         </div>
     );
 }
-
 export default Head;
---
+++ src/css/css.d.ts
@@ -0,0 +1,4 @@
+declare module '*.less'
+declare module '*.styl'
+declare module '*.css'
+declare module '*.scss'
