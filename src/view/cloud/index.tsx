7f8229e22e905be2c4d8f39bb48e6d1686692a1b
wx170903 <384313798@qq.com>
2022-04-26 16:06:34 +0800

    init

---
+++ src/view/cloud/index.tsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+
+const Home = () => {
+    return (
+        <div className="component">
+            我是首页
+        </div>
+    )
+}
+export default Home;
